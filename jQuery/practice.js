146af4947f7dcef9b2295093cb6e7e1af22b79d9
kinoshitaken123 <k1k7s9e3@gmail.com>
2021-05-27 21:39:13 +0900

    [Practice jQuery]

--- jQuery/practice.js
+++ jQuery/practice.js
@@ -1,3 +1,8 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function () {
+  $('.box1').mouseover(function () {
+    $('.box1').css({ 'background-color': '#0000FF' });
+  });
+  $('.box1').mouseout(function () {
+    $('.box1').css({ 'background-color': '#FF0000' });
+  });
 });
